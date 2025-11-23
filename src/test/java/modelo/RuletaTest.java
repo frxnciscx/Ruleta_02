4818a2d7dccd96d7b4a30b12ea6edf2ab6235c07
francisca <f.aguayo03@ufromail.cl>
2025-11-22 21:33:38 -0300

    Creacion de RuletaTest

---
+++ src/test/java/modelo/RuletaTest.java
@@ -0,0 +1,14 @@
+package modelo;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuletaTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+}
