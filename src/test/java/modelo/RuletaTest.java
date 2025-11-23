6596c2c75cc29191c2277f19eeaba50394f26ba0
francisca <f.aguayo03@ufromail.cl>
2025-11-22 21:51:59 -0300

    Pruebas unitarias

--- src/test/java/modelo/RuletaTest.java
+++ src/test/java/modelo/RuletaTest.java
@@ -6,9 +6,46 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class RuletaTest {
+    private Ruleta ruleta;
 
     @BeforeEach
     void setUp() {
+        ruleta = new Ruleta(1000);
     }
 
+    //CAS0 1
+    @Test
+    void testConstructorSaldoNegativo() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            new Ruleta(-100);
+        });
+        assertEquals("Saldo inicial negativo", exception.getMessage());
+    }
+
+    //CAS0 2
+    @Test
+    void testDepositar() {
+        int saldoInicial = ruleta.getSaldo();
+        ruleta.depositar(500);
+        assertEquals(saldoInicial + 500, ruleta.getSaldo());
+    }
+
+    //CAS0 3
+    @Test
+    void testJugarApuestaNula() {
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ruleta.jugar(null);
+        });
+        assertEquals("Apuesta obligatoria", exception.getMessage());
+    }
+
+    //CAS0 4
+    @Test
+    void testJugarSaldoInsuficiente() {
+        ApuestaBase apuestaGrande = new ApuestaRojo(5000);
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            ruleta.jugar(apuestaGrande);
+        });
+        assertEquals("Saldo insuficiente", exception.getMessage());
+    }
 }
